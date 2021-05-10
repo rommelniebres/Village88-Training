20bb6c9afec823a6deab8a10e8143c4642723d6b
Rommel Niebres <rommelniebresj@gmail.com>
2021-05-10 11:17:44 +0800

    re-oraganized folders

---
+++ PHP Track/form_validations/application/controllers/users.php
@@ -0,0 +1,30 @@
+    <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+    class Users extends CI_Controller {
+    public function new_user() {
+        $this->load->view('users/new');
+    }
+    public function create() {
+        $this->load->model('User');
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('first_name', 'First Name', 'trim|required');
+        $this->form_validation->set_rules('last_name', 'Last Name', 'trim|required');
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[users.email]');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[8]|matches[password_confirmation]');
+        $this->form_validation->set_rules('password_confirmation', 'Password Confirmation', 'trim|required');
+        if($this->form_validation->run()) {
+        $id = $this->User->create($this->input->post());
+        $success[] = 'Welcome! Registration was successful!';
+        $this->session->set_flashdata('success', $success);
+        redirect('/users/show/' . $id);
+        } else {
+        $errors = array(validation_errors());
+        $this->session->set_flashdata('errors', $errors);
+        redirect('/');
+        }
+    }
+    public function show($id) {
+        $this->load->model('User');
+        $data['user'] = $this->User->find($id);
+        $this->load->view('users/show', $data);
+    }
+    }
---
+++ PHP Track/form_validations/application/models/user.php
@@ -0,0 +1,14 @@
+    <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+    class User extends CI_Model {
+    public function create($post) {
+        $query = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at)
+                VALUES (?,?,?,?,?,?)";
+        $values = array($post['first_name'], $post['last_name'], $post['email'],
+                        md5($post['password']), date("Y-m-d, H:i:s"), date("Y-m-d, H:i:s"));
+        $id = $this->db->insert_id($this->db->query($query, $values));
+        return $id;
+    }
+    public function find($id) {
+        return $this->db->query("SELECT * FROM users WHERE id = ?", array($id))->row_array();
+    }
+    }
